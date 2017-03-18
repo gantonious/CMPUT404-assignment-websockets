e19a4d9ffa020ebc1d5ad85294ef3ee4f09ca304
George Antonious <george@antonious.ca>
2017-03-18 10:44:19 -0600

    Properly spawn of tasks when new subscriptions are made

--- sockets.py
+++ sockets.py
@@ -26,6 +26,17 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+# from: https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
 class World:
     def __init__(self):
         self.clear()
@@ -60,9 +71,19 @@ class World:
         return self.space
 
 myWorld = World()
+clients = []
+
+def build_update_event(entity, data):
+    update_payload = { "entity" : entity,
+                       "data" : data }
+
+    return json.dumps(update_payload)
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    update_event = build_update_event(entity, data)
+    for client in clients:
+        client.put(update_event)
 
 myWorld.add_set_listener( set_listener )
 
@@ -71,17 +92,45 @@ def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return redirect("/static/index.html")
 
+# from: https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            if msg is not None:
+                packet = json.loads(msg)
+                process_incoming_message(packet)
+            else:
+                break
+    except:
+        pass
 
+def process_incoming_message(message):
+    entity = update_event["entity"]
+    data = update_event["data"]
+
+    update_property_if_found(entity, 'x', data)
+    update_property_if_found(entity, 'y', data)
+    update_property_if_found(entity, 'colour', data)
+
+# from: https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print "WS Error %s" % e
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 def flask_post_json():
